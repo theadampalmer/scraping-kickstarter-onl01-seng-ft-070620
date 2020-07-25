7407feba20eca6d06801cfc5ff39ef3849ec6866
Adam Palmer <theadampalmer@gmail.com>
2020-07-25 17:49:40 +0000

    Automatically backed up by Learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -1,5 +1,7 @@
 require 'nokogiri'
 
-def create_project_hash
+    def create_project_hash
+        html = File.read('fixtures/kickstarter.html')
  
-end
+kickstarter = Nokogiri::HTML(html)
+    end
