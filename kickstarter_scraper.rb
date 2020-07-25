53a76a85c3e84b69db612c5677fab8a183d0e1dc
Adam Palmer <theadampalmer@gmail.com>
2020-07-25 17:58:07 +0000

    Automatically backed up by Learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -1,12 +1,12 @@
 equire 'nokogiri'
 require 'pry'
 
-# projects: kickstarter.css("li.project.grid_4")
-# title: project.css("h2.bbcard_name strong a").text
-# image link: project.css("div.project-thumbnail a img").attribute("src").value
-# description: project.css("p.bbcard_blurb").text
-# location: project.css("span.location-name").text
-# project.css("ul.project-stats li.first.funded strong").text
+projects: kickstarter.css("li.project.grid_4")
+title: project.css("h2.bbcard_name strong a").text
+image link: project.css("div.project-thumbnail a img").attribute("src").value
+description: project.css("p.bbcard_blurb").text
+location: project.css("span.location-name").text
+project.css("ul.project-stats li.first.funded strong").text
 
 
 html = File.read('fixtures/kickstarter.html')
